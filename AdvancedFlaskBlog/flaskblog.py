304bbb8b460fa822515458b9908dcb77601f1c77
Ervin Pepic <ervin@Ervins-MacBook-Pro-2.local>
2019-03-23 23:31:24 +0100

    Added styles and template engine

--- AdvancedFlaskBlog/flaskblog.py
+++ AdvancedFlaskBlog/flaskblog.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+	{
+		'author': 'Ervin Pepic',
+		'title': 'Blog Post 1.',
+		'content': 'Prvi post sadrzaj',
+		'date_posted': 'April 1, 2019'
+	},
+	{
+		'author': 'Emel Pepic',
+		'title': 'Blog Post 2.',
+		'content': 'Drugi post sadrzaj',
+		'date_posted': 'April 3, 2019'
+	}
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return "<h1>Hello, World!</h1>"
+	return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-	return "<h1>Hello about Page!</h1>"
+	return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
