13aa69f0010e46bb6d7859fa6d6d776bf02e1854
Ervin Pepic <ervin@Ervins-MacBook-Pro-2.local>
2019-03-28 23:49:48 +0100

    Added logic for update profile post and update profile picture

--- AdvancedFlaskBlog/flaskblog/forms.py
+++ AdvancedFlaskBlog/flaskblog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flaskblog.models import User
 
 
@@ -27,4 +29,20 @@ class LoginForm(FlaskForm):
 	remember			= BooleanField('Remeber me')
 	submit 				= SubmitField('Login')
 
+class UpdateAccountForm(FlaskForm):
+	username 			= StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+	email 				= StringField('Email', validators=[DataRequired(), Email()])
+	picture				= FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
+	submit 				= SubmitField('Update')
 
+	def validate_username(self, username):
+		if username.data != current_user.username:
+			user 	= User.query.filter_by(username=username.data).first()
+			if user:
+				raise ValidationError('User with that username already exists. Chose another...')
+
+	def validate_email(self, email):
+		if email.data != current_user.email:
+			user 	= User.query.filter_by(email=email.data).first()
+			if user:
+				raise ValidationError('Email is already used...')
